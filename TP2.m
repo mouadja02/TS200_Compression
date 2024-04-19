a6f9b287b2db644a13513c3b1369924d5869c308
mouadja02 <118407337+mouadja02@users.noreply.github.com>
2024-04-19 11:05:20 +0200

    Troisieme Commit

--- TP2.m
+++ TP2.m
@@ -1,8 +1,8 @@
 clc; close all;
 
 % Charger ou générer deux images I1 et I2
-I1 = imread("football\gray\football001.ras");
-I2 = imread("football\gray\football002.ras");
+I1 = double(imread("football\gray\football001.ras"));
+I2 = double(imread("football\gray\football002.ras"));
 
 % Convertir en niveaux de gris si ce ne sont pas des images en niveaux de gris
 if size(I1, 3) == 3
@@ -13,89 +13,23 @@ if size(I2, 3) == 3
 end
 
 % Calcul des vecteurs de mouvement
-[Is, SADmin] = blockmatch(I1, I2, 8, 15);
+[Vx,Vy] = blockmatching(I1, I2, 4, 4, 15, 15);
 
-% Dimensions des blocs
-blockSize = 8;
+bw = 4;
 
-% Extraction des composantes horizontales et verticales
-Vx = Is(:,:,1);
-Vy = Is(:,:,2);
 
-% Réduction de la taille de Vx et Vy à la grille
-% Assurez-vous que Vx et Vy correspondent exactement aux intervalles de la grille
-Vx = Vx(1:blockSize:end, 1:blockSize:end);
-Vy = Vy(1:blockSize:end, 1:blockSize:end);
 
 % Calcul de l'amplitude et de la direction
 Amplitude = sqrt(Vx.^2 + Vy.^2);
 Direction = atan2d(Vy, Vx);
 
 % Affichage des résultats
-figure;
-subplot(2,3,1), imshow(I1), title('Image I1');
-subplot(2,3,2), imshow(I2), title('Image I2');
-subplot(2,3,3), imshow(mat2gray(Amplitude)), title('Amplitude du mouvement');
-subplot(2,3,4), imshow(mat2gray(Direction)), title('Direction du mouvement');
-subplot(2,3,5), quiver(Vx, Vy, 0), axis ij, axis tight, title('Vecteurs de mouvement');
-
-% Affichage des vecteurs sur l'image
-subplot(2,3,6), imshow(I1), hold on;
-[xGrid, yGrid] = meshgrid(1:blockSize:size(I1,2), 1:blockSize:size(I1,1));
-quiver(xGrid, yGrid, Vx, Vy, 'y'), title('Vecteurs sur I1');
+figure, imshow(uint8(I1)), title('Image I1');
+figure, imshow(uint8(I2)), title('Image I2');
+figure, imshow(uint8(I1)), hold on;
+[hV,wV] = size(Vx);
+[X, Y] = meshgrid(1:wV, 1:hV);
+X = (1+bw)/2 + (X-1)*bw;
+Y= (1+bw)/2 + (Y-1)*bw;
+quiver(X, Y, Vx, Vy, 'c'), title('Vecteurs sur I1');
 hold off;
-
-
-function [motionVect] = backwardBlockMatching(I1, I2)
-    % Taille des blocs
-    blockSize = 8;
-    % Taille de la fenêtre de recherche
-    searchWindow = 15;
-
-    % Dimensions de l'image
-    [rows, cols] = size(I1);
-    
-    % Initialisation du vecteur de mouvement
-    motionVect = zeros(floor(rows/blockSize), floor(cols/blockSize), 2);
-    
-    % Parcours de chaque bloc de l'image I2
-    for i = 1:blockSize:rows-blockSize+1
-        for j = 1:blockSize:cols-blockSize+1
-            % Bloc courant dans I2
-            blockI2 = I2(i:i+blockSize-1, j:j+blockSize-1);
-            
-            % Initialisation de la meilleure correspondance
-            minSAD = inf;
-            dx = 0;
-            dy = 0;
-            
-            % Limites de la fenêtre de recherche
-            range = floor(searchWindow/2);
-            rowMin = max(1, i-range);
-            rowMax = min(rows-blockSize+1, i+range);
-            colMin = max(1, j-range);
-            colMax = min(cols-blockSize+1, j+range);
-            
-            % Recherche exhaustive dans la fenêtre autour du bloc
-            for x = rowMin:rowMax
-                for y = colMin:colMax
-                    % Bloc courant dans I1
-                    blockI1 = I1(x:x+blockSize-1, y:y+blockSize-1);
-                    
-                    % Calcul du SAD
-                    SAD = sum(abs(blockI2(:) - blockI1(:)));
-                    
-                    % Mise à jour de la meilleure correspondance
-                    if SAD < minSAD
-                        minSAD = SAD;
-                        dx = x - i;
-                        dy = y - j;
-                    end
-                end
-            end
-            
-            % Enregistrement du vecteur de mouvement pour ce bloc
-            motionVect((i-1)/blockSize+1, (j-1)/blockSize+1, :) = [dx dy];
-        end
-    end
-end
--- blockmatching.m
+++ blockmatching.m
@@ -1,22 +1,23 @@
 function [Vx,Vy]= blockmatching(I1,I2,bw,bh,fw,fh)
     [hI,wI] = size(I1);
     wV = wI / bw;
-    V = hI / bh;
+    hV = hI / bh;
     Vx = zeros(hV,wV);
     Vy = zeros(hV,wV);
     tw = (0:bw-1);
     th = (0:bh-1);
-
     for i=1:wV
         for j=1:hV
             x2 = (i-1)*bw+1;
             y2 = (j-1)*bh+1;
             B2 = I2(y2+th,x2+tw);
-            
+            crit_min = inf;
             flag = 0;
+            u_min = inf;
+            v_min = inf;
             for u = (-fw+1)/2:(fw-1)/2
                 for v = (-fh+1)/2:(fh-1)/2
-                    if (x2+u)>=1 && x2+u+hw-1<=wI && y2+v>=1 && y2+v+bh-1<=hI
+                    if (x2+u)>=1 && x2+u+bw-1<=wI && y2+v>=1 && y2+v+bh-1<=hI
                         B1 = I1(y2+v+th,x2+u+tw);
                         crit = SAD(B1,B2);
                         if flag==0 || crit<crit_min
